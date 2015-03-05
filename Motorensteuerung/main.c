9307b28ec7ef424331b45f4dab0e6fdaaf6a7a29
Angelo Conconi <angelo.conconi@gmail.com>
2015-03-05 10:13:18 +0100

    Moved to Subfolder

---
+++ Motorensteuerung/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Manuel
+ *
+ * Created on 5. März 2015, 09:59
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
